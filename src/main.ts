6a136edbe52ed34534064e471a0de9a6eb49dea3
Chamith-Digitus <chamith.h@digitustecglobal.com>
2023-08-17 21:33:24 +0530

    addf

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NoEmptyValuesPipe } from './configs/pipes/dto-validater.pipe';
+import * as process from "process"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors();
   app.useGlobalPipes(new NoEmptyValuesPipe())
-  await app.listen(8080);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
